1dd51a7969bd38fb5d08fcdfc2da62f42ba4d751
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-23 18:06:59 +0000

    Require flash gem, add user and find by session

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra/base'
-require_relative './lib/peeps.rb'
+require 'sinatra/flash'
+require './lib/peeps.rb'
+require './lib/users.rb'
+require './database_connection_setup'
 
 class Chitter < Sinatra::Base
+  enable :sessions, :method_override
+  register Sinatra::Flash
 
   get '/' do
     redirect '/chitter'
   end
 
   get '/chitter' do
+   @user = User.find(id: session[:user_id])
+   @peeps = Peeps.all.reverse
    erb :'chitter/index'
   end
 
